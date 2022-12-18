7d23c5e1272809788ff50df28bffcd7c60453cb1
terpii <56959236+terpii@users.noreply.github.com>
2022-12-18 13:56:46 +0100

    Added api key loading and and request session

--- py-xbox.py
+++ py-xbox.py
@@ -1,3 +1,28 @@
 import requests
 import argparse
 
+
+apikey = ''
+## read the api key
+try:
+    with open('api-key') as f:
+        apikey = f.readline()
+except:
+    print("No api-key found\nPlease paste your apikey into a file name 'api-key'")
+    exit(1)
+
+# api key clean ups
+apikey = apikey.replace('\n','')
+if len(apikey) < 5:
+    print('apikey is either missing or invalid, please paste your apikey into the api-key file')
+    exit(1)
+
+print("Apikey succesfully loaded")
+
+## creating a sesssion
+global session = requests.session()
+session.headers. = {
+        'x-authorization':apikey
+}
+
+
